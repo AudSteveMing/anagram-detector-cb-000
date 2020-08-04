0b7fcf73a89513c4e5706314a865d64a46e692cb
Steve Cordova <cordovas@bc.edu>
2020-08-04 22:11:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,27 @@ class Anagram
     @word = word
   end
 
-  def match(word)
-    
+  def match(array)
+
+    sorted_word = @word.sort
+    words = array.split(/( ,| )/)
+    words.each do |i|
+      if i.sort == sorted_word
+        i
+      end
+    end
+#
+#     @word == ["hello", "wood"]
+#     @word ==
+#
+#     sort @word
+#
+#     separate @word into array of letters
+#     iterate over array
+#     separate each element of array into array of single letters
+#     find.all letters from @word in new array
+#
+# [w, o , o, d]  [d, o, o, w]
+
   end
 end
