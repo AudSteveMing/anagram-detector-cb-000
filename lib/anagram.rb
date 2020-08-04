4fa9e3bf671f6b61cebfb76482d9fbb837a8c08b
Steve Cordova <cordovas@bc.edu>
2020-08-04 22:56:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,37 +10,18 @@ class Anagram
   end
 
   def match(anagram_match)
-    count = 0
     split_word = @word.split("") #convert instance string to array of individual letters
     # split_array = array.split(/( ,| )/) #split array of provided words into indiviudal elements
     match = ""
     anagram_match.each do |i|
-      array_words = i.split("")
+      array_words = i.split("") #convert iteration string to array of individual letters
       if array_words.sort == split_word.sort
          match = i
        end
-       match
       # binding.pry
     end
-
-    #   if i.sort("") == split_word.sort
-    #     binding.pry
-    #   end
-    # end
-#
-#     @word == ["hello", "wood"]
-#     @word ==
-#
-#     sort @word
-#
-#     separate @word into array of letters
-#     iterate over array
-#     separate each element of array into array of single letters
-#     find.all letters from @word in new array
-#
-# [w, o , o, d]  [d, o, o, w]
+    match
 
   end
-  # binding.pry
 
 end
