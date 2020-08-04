74933d3ae0551c8ba0200504daede53a29db5fea
Steve Cordova <cordovas@bc.edu>
2020-08-04 22:57:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,11 +12,11 @@ class Anagram
   def match(anagram_match)
     split_word = @word.split("") #convert instance string to array of individual letters
     # split_array = array.split(/( ,| )/) #split array of provided words into indiviudal elements
-    match = ""
+    match = []
     anagram_match.each do |i|
       array_words = i.split("") #convert iteration string to array of individual letters
       if array_words.sort == split_word.sort
-         match = i
+         match << i
        end
       # binding.pry
     end
